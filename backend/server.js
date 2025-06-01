457aff2dda5d9cb35ac5aa872da06f45d7bf540d
akbrx <strongstroomer@gmail.com>
2025-06-01 18:48:59 +0700

    update ubah nama database

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import router from './routes/index.js';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-//import Users from './user/user-model.js';
+//import Users from './users/user-model.js';
 
 dotenv.config();
 const app = express();
